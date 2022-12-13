6fcb8982694a42c7f1d3d2a2cbb4afd803d93555
Miao, ZhiCheng <miao@superfluid.finance>
2022-12-13 22:49:07 +0200

    added initial nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Nix flake for the rcfmm monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakeUtils.url = "github:numtide/flake-utils";
+    # foundry.url = "github:shazow/foundry.nix";
+  };
+
+  outputs = { self, nixpkgs, flakeUtils } : flakeUtils.lib.eachDefaultSystem 
+  (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+    minimumInputs = with pkgs; [
+      nodejs-18_x
+      nodePackages.pnpm
+    ];
+    packageMathInputs = with pkgs; [
+      # sage math for symbolic calculation
+      sage
+    ];
+    packageToyModelInputs = with pkgs; [
+      # haskell build tools
+      cabal-install
+      haskell.compiler.ghc94
+      # haskell dev tools
+      hlint
+      stylish-haskell
+      # other tools
+      gnuplot
+    ];
+  in {
+    devShells.default = with pkgs; mkShell {
+      buildInputs = minimumInputs
+        ++ packageMathInputs
+        ++ packageToyModelInputs;
+    };
+  });
+}
